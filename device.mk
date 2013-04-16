6f0c2ae6f1f2dd0924c6da88e52ecce78c719dc5
Andrew Dodd <atd7@cornell.edu>
2013-04-16 01:19:11 -0400

    find5: Add/prefer xxhdpi resources
    Change-Id: I84324cc1de767451b3a99fc8a2299ae4a08f8287

--- device.mk
+++ device.mk
@@ -25,8 +25,8 @@ DEVICE_PACKAGE_OVERLAYS := device/oppo/find5/overlay
 # currently contain all of the bitmaps at xhdpi density so
 # we do this little trick to fall back to the hdpi version
 # if the xhdpi doesn't exist.
-PRODUCT_AAPT_CONFIG := normal hdpi xhdpi
-PRODUCT_AAPT_PREF_CONFIG := xhdpi
+PRODUCT_AAPT_CONFIG := normal hdpi xhdpi xxhdpi
+PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
 PRODUCT_PACKAGES := \
 	lights.msm8960
