94264be9e989832223915446befe0b500019c3db
maxwen <max.weninger@gmail.com>
2013-07-25 00:41:28 +0200

    added our own init.rc

--- device.mk
+++ device.mk
@@ -57,6 +57,7 @@ PRODUCT_COPY_FILES += \
 	device/oppo/find5/thermald-find5.conf:system/etc/thermald.conf
 
 PRODUCT_COPY_FILES += \
+	device/oppo/find5/configs/init.rc:root/init.rc \
 	device/oppo/find5/configs/init.find5.rc:root/init.find5.rc \
 	device/oppo/find5/configs/init.find5.usb.rc:root/init.find5.usb.rc \
 	device/oppo/find5/configs/init.recovery.find5.rc:root/init.recovery.find5.rc \
