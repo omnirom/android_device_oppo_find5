edd93e780dcd67dd14644cf366a97c5c9cfbf769
maxwen <max.weninger@gmail.com>
2013-08-29 22:40:46 +0200

    remove device local wpa_supplicant.conf

--- Android.mk
+++ Android.mk
@@ -27,14 +27,6 @@ ifneq ($(filter find5,$(TARGET_DEVICE)),)
 
 LOCAL_PATH := $(call my-dir)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE       := wpa_supplicant.conf
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := $(LOCAL_MODULE)
-LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/wifi
-include $(BUILD_PREBUILT)
-
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
 endif
