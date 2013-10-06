a15b1fb5ce32fbaaf37a0d3765ca3f4de8ae2dcb
maxwen <max.weninger@gmail.com>
2013-10-06 15:15:20 +0200

    find5: workaround UI glitch caused by lcd_density
    Using ro.sf.lcd_density=480 causes an UI glitch that
    can bee seen here http://www.mediafire.com/view/fdhkey32dn3s99z/Screenshot_2013-09-24-10-47-34.png
    This should be just temporary until the base reason is found
    
    Change-Id: I3a25d07142727149723895ed081cee9299035698

--- device.mk
+++ device.mk
@@ -129,7 +129,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 	ro.opengles.version=131072
 
 PRODUCT_PROPERTY_OVERRIDES += \
-	ro.sf.lcd_density=480
+	ro.sf.lcd_density=460
 
 # qcom
 PRODUCT_PROPERTY_OVERRIDES += \
